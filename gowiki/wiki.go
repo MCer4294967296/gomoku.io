9da35b9782de859453e546cc2a3d58c4756cc814
Xiangyu Cai <xiangyu.cai@berkeley.edu>
2020-06-16 16:30:40 -0700

    gowiki: data structures; include *.txt in .gitignore

--- gowiki/wiki.go
+++ gowiki/wiki.go
@@ -1,19 +1,32 @@
 package main
 
-
 import (
 	"fmt"
-	"net/http"
-	//"io/ioutil"
+	"io/ioutil"
 )
 
-
-func main() {
-	http.HandleFunc("/", HelloServer)
-	_ = http.ListenAndServe(":8000", nil)
+type Page struct {
+	Title string
+	Body  []byte
 }
 
+func main() {
+	p1 := &Page{Title: "TestPage", Body: []byte("This is a sample Page.")}
+	_ = p1.save()
+	p2, _ := loadPage("TestPage")
+	fmt.Println(string(p2.Body))
+}
 
-func HelloServer (w http.ResponseWriter, r *http.Request) {
-	_, _ = fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
+func (p *Page) save() error {
+	filename := p.Title + ".txt"
+	return ioutil.WriteFile(filename, p.Body, 0600)
+}
+
+func loadPage(title string) (*Page, error) {
+	filename := title + ".txt"
+	body, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	return &Page{Title: title, Body: body}, nil
 }
